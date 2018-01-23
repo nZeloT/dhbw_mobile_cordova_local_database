3de14d944249a1ba68e45b4e50a87ab0d787b989
nZeloT <leontsteiner@gmail.com>
2018-01-23 13:19:32 +0100

    Working SQLITE Plugin

--- www/js/index.js
+++ www/js/index.js
@@ -27,23 +27,84 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-        window.sqlitePlugin.selfTest(function() {
-            console.log('SELF test OK');
+        let that = this;
+
+        window.sqlitePlugin.openDatabase({
+            name: 'thisIsMyDummyDB.db',
+            location: 'default'
+        }, function(db) {
+            console.log("Database open.")
+            that.createTable(db);
+        }, function (error){
+          document.getElementById("deviceready").appendChild(that.createTextItem("Oo -.- " + error));
         });
     },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    createTable: function(db) {
+        let that = this;
+        db.transaction(function(tr){
+          tr.executeSql('CREATE TABLE IF NOT EXISTS person (firstname, lastname)');
+        }, function(error) {
+            console.log(error);
+        }, function() {
+            console.log("Table created");
+            that.insertItem(db, "Ich", "Du");
+        })
+    },
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    selectItems: function(db){
+        let that = this;
+        db.transaction(function(tr){
+          tr.executeSql('SELECT firstname, lastname FROM person', [], function (tr, rs) {
+              for(let x = 0; x < rs.rows.length; x++) {
+                let text = rs.rows.item(x).firstname + ", " + rs.rows.item(x).lastname;
+                document.getElementById("deviceready").appendChild(
+                        that.createTextItem(text));
+              }
+          });
+        }, function(error){
+            console.log(error);
+        }, function() {
+            that.closeDatabase(db);
+        })
+    },
 
-        console.log('Received Event: ' + id);
+    insertItem: function(db, first, last) {
+        let that = this;
+        console.log("on insert");
+      db.transaction(function (tx) {
+          console.log("begin insert trans");
+        let query = "INSERT INTO person (firstname, lastname) VALUES (?,?)";
+
+        tx.executeSql(query, [first, last], function(tx, res) {
+              console.log("insertId: " + res.insertId + " -- probably 1");
+              console.log("rowsAffected: " + res.rowsAffected + " -- should be 1");
+            },
+            function(tx, error) {
+              console.log('INSERT error: ' + error.message);
+            }
+        );
+
+        console.log("query exec");
+      }, function(error) {
+        console.log('transaction error: ' + error.message);
+      }, function() {
+        console.log('transaction ok');
+        that.selectItems(db);
+      });
+    },
+
+    closeDatabase: function(db) {
+        db.close();
+    },
+
+    createTextItem: function(text){
+        let node = document.createElement("li");
+        let nodeText = document.createTextNode(text);
+        node.appendChild(nodeText);
+        return node;
     }
+
 };
 
 app.initialize();
